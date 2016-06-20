f933b1100d91233903b4c87b1d48412baa6b8efa
Daniel Espinoza-hernandez <d_espinoza_hernandez@yahoo.com>
2016-06-20 14:55:09 -0500

    game folder for group and assets

---
+++ THE GAME/Assets/Scripts/LoginMenuScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoginMenuScript : MonoBehaviour {
+    public Button submit;
+    public Button guestLogin;
+    public Button pwdReset;
+    public Button register;
+    public Button back;
+	// Use this for initialization
+	void Start () {
+        submit = submit.GetComponent<Button>();
+        guestLogin = guestLogin.GetComponent<Button>();
+        pwdReset = pwdReset.GetComponent<Button>();
+        register = register.GetComponent<Button>();
+        back = back.GetComponent<Button>();
+	}
+
+    public void submitPressed() {
+        //do something with login info
+    }
+
+    public void guestLoginPressed() {
+        //continue without saving
+    }
+
+    public void pwdResetPressed() {
+        //go to password reset page
+    }
+
+    public void registerPressed() {
+        //go to registration page
+    }
+
+    public void backPressed() {
+        SceneManager.LoadScene(0);
+    }
+	
+}
