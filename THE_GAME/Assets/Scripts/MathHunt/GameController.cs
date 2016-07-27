9232e6baa1028fcc42cc205407e3d2a55f2f8774
Brian G <briantrumpet3@gmail.com>
2016-07-27 14:57:10 -0500

    Commit!
    Fixed Bugs

---
+++ THE_GAME/Assets/Scripts/MathHunt/GameController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+	public Transform[] spawns;
+	public GameObject[] bird;
+	public float speed = 1.0f;
+	public int level = 1;
+	int rand;
+	int count;
+	public float speedx;
+
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (SpawnWaves ());
+	}
+
+	IEnumerator SpawnWaves ()
+	{
+		yield return new WaitForSeconds (startWait);
+		while (true)
+		{
+			for (int i = 0; i < hazardCount; i++)
+			{
+				if (level == 1) {
+					rand = Random.Range (0, 2);
+					count = Random.Range (0, 2);
+					if(spawns[count].position.x < 0){
+						GameObject obj = Instantiate (bird [rand], spawns [count].position, spawns [count].rotation) as GameObject;
+						obj.GetComponent<DeathByTime>().Initialize(Vector2.right,speedx);
+					}else{	
+						var offset = spawns [count].rotation;
+						offset.y = 180;
+						GameObject obj2 = Instantiate (bird [rand], spawns [count].position, offset) as GameObject;
+						obj2.GetComponent<DeathByTime>().Initialize(Vector2.left,speedx);
+
+					}
+				}
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+		}
+	}
+		
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+}
