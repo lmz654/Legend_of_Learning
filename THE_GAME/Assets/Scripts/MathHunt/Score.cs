0597ba335464ae88d513645e237f99f97554ad30
Brian G <briantrumpet3@gmail.com>
2016-07-27 14:34:53 -0500

    Merge branch 'lmz654/master'
    Second Commit

---
+++ THE_GAME/Assets/Scripts/MathHunt/Score.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+	public int score;
+	public GUIText scoreText;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+		updateScore ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void addScore(int x){
+		score += x;
+		updateScore ();
+	}
+
+	void updateScore(){
+		scoreText.text = "Score: " + score;
+	}
+		
+
+}
