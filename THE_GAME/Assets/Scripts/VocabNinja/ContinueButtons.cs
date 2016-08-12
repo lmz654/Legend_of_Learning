bb0caa7df5a7830e80f62d89c206ae76bfac0a15
d-espinozahernandez <d_espinoza_hernandez@yahoo.com>
2016-08-12 15:08:00 -0500

    finished vocab ninja

--- THE_GAME/Assets/Scripts/VocabNinja/ContinueButtons.cs
+++ THE_GAME/Assets/Scripts/VocabNinja/ContinueButtons.cs
@@ -4,16 +4,15 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ContinueButtons : MonoBehaviour {
-    public GameObject yes;
-    public GameObject no;
+    
     public Button contYes;
     public Button contNo;
+    
     // Use this for initialization
     void Start () {
         contYes = contYes.GetComponent<Button>();
         contNo = contNo.GetComponent<Button>();
-        yes.SetActive(false);
-        no.SetActive(false);
+        
     }
 
     public void yesPressed()
